9469e66cc89f8c8c7ab9cad172c2ab0b3670aed4
Kin <ccsckin@gmail.com>
2025-11-07 22:13:47 +0800

    exercise 5.2

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -15,14 +15,26 @@ const App = () => {
     )  
   }, [])
 
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedUserJSON')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+    }
+  }, [])
+  
   const handleLogin = async (event) => {
     event.preventDefault()
-    // console.log(`login in with ${username} and ${password}`)
     const user = await loginService.login({ username, password })
     setUser(user)
     setUsername('')
     setPassword('')
-    console.log('user', user)
+    window.localStorage.setItem('loggedUserJSON', JSON.stringify(user))
+  }
+
+  const logout = () => {
+    window.localStorage.removeItem('loggedUserJSON')
+    setUser(null)
   }
 
   const loginForm = () => (
@@ -55,7 +67,12 @@ const App = () => {
   const blogList = () => (
     <div>
       <h2>blogs</h2>
-      <p>{user.username} logged in</p>
+      <p>
+        {user.username} logged in
+        <button type='button' onClick={logout}>
+          logout
+        </button>
+      </p>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
