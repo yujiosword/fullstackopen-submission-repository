68e432acd1f8d137c774b32289db599db2dee0ff
Kin <ccsckin@gmail.com>
2025-12-31 10:54:37 +0800

    exercise 5.18

--- part5/bloglist-playwright/playwright.config.js
+++ part5/bloglist-playwright/playwright.config.js
@@ -15,19 +15,19 @@ import { defineConfig, devices } from '@playwright/test';
 export default defineConfig({
   testDir: './tests',
   /* Run tests in files in parallel */
-  fullyParallel: true,
+  // fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  // workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
-    // baseURL: 'http://localhost:3000',
+    baseURL: 'http://localhost:5173',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
@@ -70,7 +70,9 @@ export default defineConfig({
     //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
     // },
   ],
-
+  timeout: 3000,
+  fullyParallel: false,
+  workers: 1,
   /* Run your local dev server before starting the tests */
   // webServer: {
   //   command: 'npm run start',
--- part5/bloglist-playwright/tests/blog_app.spec.js
+++ part5/bloglist-playwright/tests/blog_app.spec.js
@@ -1,8 +1,16 @@
 const { test, expect, beforeEach, describe } = require('@playwright/test')
 
 describe('Blog app', () => {
-  beforeEach(async ({ page }) => {
-    await page.goto('http://localhost:5173/')
+  beforeEach(async ({ page, request }) => {
+    await request.post('api/testing/reset')
+    await request.post('api/users', {
+      data: {
+        name: 'kinkin',
+        username: 'kinkin',
+        password: '1234'
+      }
+  })
+    await page.goto('/')
   })
 
   test('Login form is shown', async({ page }) => {
@@ -10,5 +18,24 @@ describe('Blog app', () => {
     await expect(page.getByLabel('username')).toBeVisible()
     await expect(page.getByLabel('password')).toBeVisible()
     await expect(page.getByRole('button', { name: 'login' })).toBeVisible()
+  })
+
+  describe('Login', () => {
+    test('succeeds with correct credentials', async ({ page }) => {
+      await page.getByLabel('username').fill('kinkin')
+      await page.getByLabel('password').fill('1234')
+      await page.getByRole('button', { name: 'login' }).click()
+
+      await expect(page.getByText('kinkin logged in')).toBeVisible()
+    })
+
+    test('fails with wrong credentials', async ({ page }) => {
+      await page.getByLabel('username').fill('kin')
+      await page.getByLabel('password').fill('1234')
+      await page.getByRole('button', { name: 'login' }).click()
+
+      await expect(page.getByText('wrong username or password')).toBeVisible()
+      await expect(page.getByText('kin logged in')).not.toBeVisible()
+    })
   })
 })
