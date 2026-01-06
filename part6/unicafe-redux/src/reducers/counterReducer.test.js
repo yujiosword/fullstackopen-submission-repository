983967f12d504b6d30c7fc8249e0a43f5fb6550c
Kin <ccsckin@gmail.com>
2026-01-06 20:57:56 +0800

    exercise 6.1

--- part6/unicafe-redux/src/reducers/counterReducer.test.js
+++ part6/unicafe-redux/src/reducers/counterReducer.test.js
@@ -32,4 +32,54 @@ describe('unicafe reducer', () => {
       bad: 0
     })
   })
+
+  test('ok is incremented', () => {
+    const action = {
+      type: 'OK'
+    }
+    const state = initialState
+
+    deepFreeze(state)
+    const newState = counterReducer(state, action)
+    expect(newState).toEqual({
+      good: 0,
+      ok: 1,
+      bad: 0
+    })
+  })
+
+  test('bad is incremented', () => {
+    const action = {
+      type: 'BAD'
+    }
+    const state = initialState
+
+    deepFreeze(state)
+    const newState = counterReducer(state, action)
+    expect(newState).toEqual({
+      good: 0,
+      ok: 0,
+      bad: 1
+    })
+  })
+
+  test('state is reset', () => {
+    const action = {
+      type: 'RESET'
+    }
+    const state = {
+      good: 100,
+      ok: 10,
+      bad: 0
+    }
+
+    deepFreeze(state)
+
+    const newState = counterReducer(state, action)
+    expect(newState).toEqual({
+      good: 0,
+      ok: 0,
+      bad: 0
+    })
+  })
 })
